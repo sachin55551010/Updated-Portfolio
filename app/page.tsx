317ba22e4435df53d9e14aff35a938e366b17624
Sachin Khatri <sachinbansal3333@gmail.com>
2026-07-01 16:17:00 +0530

    minor change

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
 
   return (
     <main className="h-dvh w-dvw flex flex-col items-center overflow-x-hidden">
-      {/* {resolvedTheme && <ThemeBgAnimation />} */}
+      {resolvedTheme && <ThemeBgAnimation />}
 
       <HeroSection />
     </main>
